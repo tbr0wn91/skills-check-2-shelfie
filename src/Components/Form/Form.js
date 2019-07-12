97e38aca4d22ab42acdf7d3abe853f1397becce2
thomas brown <thomasbrown9101@gmail.com>
2019-07-12 11:26:54 -0700

    created form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,56 @@
 import React, {Component} from 'react';
 
 export default class Form extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            productImage: "",
+            productName: "",
+            productPrice: 0
+            
+        }
+      
+    }
+
+
+
+    handleProductImage(productImage){
+        this.setState({
+            productImage: productImage
+        });
+    }
+
+    handleProductName(productName){
+        this.setState({
+            productName: productName
+        });
+    }
+
+    handleProductPrice(productPrice){
+        this.setState({
+            productPrice: productPrice
+        });
+    }
+
+    handleCancel = () => {
+        this.setState({
+            productImage : "",
+            productName : "",
+            productPrice : 0
+        })
+        
+    }
+
     render(){
         return (
             <div>
                 Form Component
+                <input onChange={(e) => this.handleProductImage(e.target.value)} value={this.state.productImage} type="text"/>
+                <input onChange={(e) => this.handleProductName(e.target.value)} value={this.state.productName} type="text"/>
+                <input onChange={(e) => this.handleProductPrice(e.target.value)} value={this.state.productPrice} type="number"/>
+                <button>Add to Inventory</button>
+                <button onClick={(e) => (this.handleCancel(e))}>Cancel</button>
             </div>
         )
     }
